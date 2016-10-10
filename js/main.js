f9005be4256f50757ea6d475b41dbeaa0eeac07f
diamant1991 <diamant1988@mail.ru>
2016-10-10 23:18:30 +0300

    first files

---
+++ js/main.js
@@ -0,0 +1,1 @@
+$(".img-move").twentytwenty(),$("#works-slider").length&&$("#works-slider").bxSlider({pagerCustom:"#works-thumb"}),$(".main-slider").length&&$(".main-slider").bxSlider({adaptiveHeight:!0,controls:!1}),$("#partners-carousel").owlCarousel({loop:!0,margin:20,dots:!1,nav:!0,responsive:{0:{items:1},600:{items:3},1e3:{items:5}}});
